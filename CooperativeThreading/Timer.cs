6f57209b1316365ad92134e4abead8037a87b68c
Lobsang Sanchez <lobsang.sanchez@polygon.us>
2023-05-19 08:50:13 -0500

    Update

--- CooperativeThreading/Timer.cs
+++ CooperativeThreading/Timer.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using System.Collections;
+using System.Globalization;
 
 namespace ModernWestern
 {
@@ -34,256 +35,256 @@ namespace ModernWestern
             Backward = backward;
         }
 
-        public Coroutine Start(Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
             return routine = mono.StartCoroutine(Routine(null, null, completed));
         }
 
-        public Coroutine Start(Action completed, GameObject gameObject)
+        public Coroutine Start(Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
             return routine = mono.StartCoroutine(Routine(null, null, completed));
         }
 
-        public Coroutine Start(Action<float> time, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action<float> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
             return routine = mono.StartCoroutine(Routine(null, time, completed));
         }
 
-        public Coroutine Start(Action<float> time, Action completed, GameObject gameObject)
+        public Coroutine Start(Action<float> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
             return routine = mono.StartCoroutine(Routine(null, time, completed));
         }
 
-        public Coroutine Start(Action<string> time, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action<string> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            return routine = mono.StartCoroutine(Routine(null, f => time?.Invoke(f.ToString()), completed));
+            return routine = mono.StartCoroutine(Routine(null, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed));
         }
 
-        public Coroutine Start(Action<string> time, Action completed, GameObject gameObject)
+        public Coroutine Start(Action<string> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            return routine = mono.StartCoroutine(Routine(null, f => time?.Invoke(f.ToString()), completed));
+            return routine = mono.StartCoroutine(Routine(null, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed));
         }
 
-        public Coroutine Start(Action awake, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action awake, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
             return routine = mono.StartCoroutine(Routine(awake, null, completed));
         }
 
-        public Coroutine Start(Action awake, Action completed, GameObject gameObject)
+        public Coroutine Start(Action awake, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
             return routine = mono.StartCoroutine(Routine(awake, null, completed));
         }
 
-        public Coroutine Start(Action awake, Action<float> time, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action awake, Action<float> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
             return routine = mono.StartCoroutine(Routine(awake, time, completed));
         }
 
-        public Coroutine Start(Action awake, Action<float> time, Action completed, GameObject gameObject)
+        public Coroutine Start(Action awake, Action<float> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
             return routine = mono.StartCoroutine(Routine(awake, time, completed));
         }
 
-        public Coroutine Start(Action awake, Action<string> time, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action awake, Action<string> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            return routine = mono.StartCoroutine(Routine(awake, f => time?.Invoke(f.ToString()), completed));
+            return routine = mono.StartCoroutine(Routine(awake, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed));
         }
 
-        public Coroutine Start(Action awake, Action<string> time, Action completed, GameObject gameObject)
+        public Coroutine Start(Action awake, Action<string> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            return routine = mono.StartCoroutine(Routine(awake, f => time?.Invoke(f.ToString()), completed));
+            return routine = mono.StartCoroutine(Routine(awake, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed));
         }
 
-        public Coroutine Start(Func<bool> stopWhen, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Func<bool> stopWhen, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
             return routine = mono.StartCoroutine(Routine(null, stopWhen, null, completed));
         }
 
-        public Coroutine Start(Func<bool> stopWhen, Action completed, GameObject gameObject)
+        public Coroutine Start(Func<bool> stopWhen, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
             return routine = mono.StartCoroutine(Routine(null, stopWhen, null, completed));
         }
 
-        public Coroutine Start(Func<bool> stopWhen, Action<float> time, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Func<bool> stopWhen, Action<float> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
             return routine = mono.StartCoroutine(Routine(null, stopWhen, time, completed));
         }
 
-        public Coroutine Start(Func<bool> stopWhen, Action<float> time, Action completed, GameObject gameObject)
+        public Coroutine Start(Func<bool> stopWhen, Action<float> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
             return routine = mono.StartCoroutine(Routine(null, stopWhen, time, completed));
         }
 
-        public Coroutine Start(Func<bool> stopWhen, Action<string> time, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Func<bool> stopWhen, Action<string> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            return routine = mono.StartCoroutine(Routine(null, stopWhen, f => time?.Invoke(f.ToString()), completed));
+            return routine = mono.StartCoroutine(Routine(null, stopWhen, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed));
         }
 
-        public Coroutine Start(Func<bool> stopWhen, Action<string> time, Action completed, GameObject gameObject)
+        public Coroutine Start(Func<bool> stopWhen, Action<string> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            return routine = mono.StartCoroutine(Routine(null, stopWhen, f => time?.Invoke(f.ToString()), completed));
+            return routine = mono.StartCoroutine(Routine(null, stopWhen, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed));
         }
 
-        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action completed, MonoBehaviour mono)
+        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(null, stopWhen, null, completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(null, stopWhen, null, completed)), mono);
         }
 
-        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action completed, GameObject gameObject)
+        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(null, stopWhen, null, completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(null, stopWhen, null, completed)), mono);
         }
 
-        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<float> time, Action completed, MonoBehaviour mono)
+        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<float> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(null, stopWhen, time, completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(null, stopWhen, time, completed)), mono);
         }
 
-        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<float> time, Action completed, GameObject gameObject)
+        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<float> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(null, stopWhen, time, completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(null, stopWhen, time, completed)), mono);
         }
 
-        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<string> time, Action completed, MonoBehaviour mono)
+        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<string> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(null, stopWhen, f => time?.Invoke(f.ToString()), completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(null, stopWhen, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed)), mono);
         }
 
-        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<string> time, Action completed, GameObject gameObject)
+        public void Prepare(Func<bool> startWhen, Func<bool> stopWhen, Action<string> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(null, stopWhen, f => time?.Invoke(f.ToString()), completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(null, stopWhen, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed)), mono);
         }
 
-        public Coroutine Start(Action awake, Func<bool> stopWhen, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
             return routine = mono.StartCoroutine(Routine(awake, stopWhen, null, completed));
         }
 
-        public Coroutine Start(Action awake, Func<bool> stopWhen, Action completed, GameObject gameObject)
+        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
             return routine = mono.StartCoroutine(Routine(awake, stopWhen, null, completed));
         }
 
-        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<float> time, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<float> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
             return routine = mono.StartCoroutine(Routine(awake, stopWhen, time, completed));
         }
 
-        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<float> time, Action completed, GameObject gameObject)
+        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<float> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
             return routine = mono.StartCoroutine(Routine(awake, stopWhen, time, completed));
         }
 
-        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<string> time, Action completed, MonoBehaviour mono)
+        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<string> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            return routine = mono.StartCoroutine(Routine(awake, stopWhen, f => time?.Invoke(f.ToString()), completed));
+            return routine = mono.StartCoroutine(Routine(awake, stopWhen, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed));
         }
 
-        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<string> time, Action completed, GameObject gameObject)
+        public Coroutine Start(Action awake, Func<bool> stopWhen, Action<string> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            return routine = mono.StartCoroutine(Routine(awake, stopWhen, f => time?.Invoke(f.ToString()), completed));
+            return routine = mono.StartCoroutine(Routine(awake, stopWhen, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed));
         }
 
-        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action completed, MonoBehaviour mono)
+        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(awake, stopWhen, null, completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(awake, stopWhen, null, completed)), mono);
         }
 
-        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action completed, GameObject gameObject)
+        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(awake, stopWhen, null, completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(awake, stopWhen, null, completed)), mono);
         }
 
-        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<float> time, Action completed, MonoBehaviour mono)
+        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<float> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(awake, stopWhen, time, completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(awake, stopWhen, time, completed)), mono);
         }
 
-        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<float> time, Action completed, GameObject gameObject)
+        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<float> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(awake, stopWhen, time, completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(awake, stopWhen, time, completed)), mono);
         }
 
-        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<string> time, Action completed, MonoBehaviour mono)
+        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<string> time, Action<float> completed, MonoBehaviour mono)
         {
             this.mono = mono;
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(awake, stopWhen, f => time?.Invoke(f.ToString()), completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(awake, stopWhen, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed)), mono);
         }
 
-        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<string> time, Action completed, GameObject gameObject)
+        public void Prepare(Action awake, Func<bool> startWhen, Func<bool> stopWhen, Action<string> time, Action<float> completed, GameObject gameObject)
         {
             mono ??= gameObject.GetComponent<MonoBehaviour>();
 
-            DelayUntil.Start(startWhen, () => { mono.StartCoroutine(Routine(awake, stopWhen, f => time?.Invoke(f.ToString()), completed)); }, mono);
+            DelayUntil.Start(startWhen, () => mono.StartCoroutine(Routine(awake, stopWhen, f => time?.Invoke(f.ToString(CultureInfo.InvariantCulture)), completed)), mono);
         }
 
         public void Stop()
@@ -294,7 +295,7 @@ namespace ModernWestern
             }
         }
 
-        private IEnumerator Routine(Action awake, Action<float> time, Action completed)
+        private IEnumerator Routine(Action awake, Action<float> time, Action<float> completed)
         {
             awake?.Invoke();
 
@@ -305,10 +306,10 @@ namespace ModernWestern
                 yield return Second;
             }
 
-            completed?.Invoke();
+            completed?.Invoke(count);
         }
 
-        private IEnumerator Routine(Action awake, Func<bool> stopWhen, Action<float> time, Action completed)
+        private IEnumerator Routine(Action awake, Func<bool> stopWhen, Action<float> time, Action<float> completed)
         {
             awake?.Invoke();
 
@@ -319,7 +320,7 @@ namespace ModernWestern
                 yield return Second;
             }
 
-            completed?.Invoke();
+            completed?.Invoke(count);
 
             routine = null;
         }
